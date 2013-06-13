8b003267d43b9755b1336739eb09faeaa18804a2
Ignacio Feijoo <ignacionf@gmail.com>
2013-06-13 13:07:51 -0300

    - all in class - config validation - full use of templates - templates in config - IP validation

--- gen_lb_config.py
+++ gen_lb_config.py
@@ -1,5 +1,7 @@
 import sys
 from mako.template import Template
+import ConfigParser
+import socket
 
 ### Static Variables ###
 KEEP_ALIVE_PORT = "8080"
@@ -12,172 +14,131 @@ OWNER_PROTOCOL = "tcp"
 REDIRECT_PORT = "80"
 REDIRECT_PROTOCOL = "tcp"
 
-### Global Variables ###
-input_file = ""
-redirect_url = ""
-vip = ""
-vip_last_octet = ""
-app_name = ""
-env = ""
-ip_array = []
-ip_last_octet_array = []
-order_array = []
+class IP:
+	"""IP Class"""
 
-### Functions definition ###
+	def __init__(self, addr):
+		# Validate IP Address
+		try:
+			socket.inet_aton(addr)
+			self.addr = addr
+		except socket.error:
+			raise ValueError("IP '%s' not valid" % addr)
+	
+	def __str__(self):
+		return self.addr
+	
+	def octet(self):
+		"""return the address splited"""
+		return self.addr.split(".")
+			
+
+class CiscoCSS:
+
+	def __init__(self,configFile):
+		self.config = ConfigParser.ConfigParser()
+		self.config.read(configFile)
+
+		# Validate all config keys
+		try:
+			self.vip_ip = IP(self.config.get("Global","vip_ip"))
+			self.redirect_url = self.config.get("Global","redirect_url")
+			self.env = self.config.get("Global","env")
+			self.app_name = self.config.get("Global","app_name")
+			self.service_ip = [IP(addr.strip()) for addr in self.config.get("Global","service_ip").split(",")]
+
+			self.template_keepalive = self.config.get("Template","template_keepalive")
+			self.template_service = self.config.get("Template","template_service")
+			self.template_group = self.config.get("Template","template_group")
+			self.template_owner = self.config.get("Template","template_owner")
+		except ConfigParser.NoOptionError, err:
+			raise ValueError(err)
+		except ConfigParser.NoSectionError, err:
+			raise ValueError(err)
+	
+	def printConfig(self):
+		"""debug only"""
+		print "vip_ip = ",self.vip_ip
+		print "redirect_url = ",self.redirect_url
+		print "env =",self.env 
+		print "app_name =",self.app_name
+		print "service_ip =",self.service_ip
+
+	def getKeepAlive(self,index):
+		template = Template(filename=self.template_keepalive)
+
+		return template.render(ip_last_octet=self.service_ip[index].octet()[3],
+        	                env=self.env,
+        	                order=index+1,
+        	                appname=self.app_name,
+       	                	ip=self.service_ip[index],
+        	                ka_port=KEEP_ALIVE_PORT,
+        	                ka_type=KEEP_ALIVE_TYPE,
+        	                ka_uri=KEEP_ALIVE_URI)
+
+	def getService(self,index):
+		template = Template(filename=self.template_service)
+		return template.render(ip_last_octet=self.service_ip[index].octet()[3],
+			env=self.env,
+       	                order=index+1,
+       	                appname=self.app_name,
+       	                ip=self.service_ip[index],
+			svc_proto=SERVICE_PROTOCOL)
+
+	def getGroup(self):
+		template = Template(filename=self.template_group)
+		return template.render(ip_last_octet=self.vip_ip.octet()[3],
+			env=self.env,
+       	                appname=self.app_name,
+       	                ip=self.vip_ip,
+       	                service_ip_1="%s_%s1_ka_%s" % (self.service_ip[0].octet()[3],self.env,self.app_name),
+       	                service_ip_2="%s_%s2_ka_%s" % (self.service_ip[1].octet()[3],self.env,self.app_name),
+			)
+
+	def getOwner(self):
+		template = Template(filename=self.template_owner)
+		return template.render(vip_last_octet=self.vip_ip.octet()[3],
+			env=self.env,
+       	                app_name=self.app_name,
+       	                vip_ip=self.vip_ip,
+       	                service_1="%s_%s1_ka_%s" % (self.service_ip[0].octet()[3],self.env,self.app_name),
+       	                service_2="%s_%s2_ka_%s" % (self.service_ip[1].octet()[3],self.env,self.app_name),
+			redirect_url=self.redirect_url,
+			STICKY_INACT_TIMEOUT=STICKY_INACT_TIMEOUT,
+			OWNER_PORT=OWNER_PORT,
+			OWNER_PROTOCOL=OWNER_PROTOCOL,
+			REDIRECT_PORT=REDIRECT_PORT,
+			REDIRECT_PROTOCOL=REDIRECT_PROTOCOL,
+			)
+
+		
+	def printAll(self):
+
+		for i in range(len(self.service_ip)):
+			print self.getKeepAlive(i)
+			print self.getService(i)
+
+		print self.getGroup()
+		print self.getOwner()
 
 
-def print_keep_alive(ip_last_octet, ip, order, env, app_name, KEEP_ALIVE_PORT,
-                     KEEP_ALIVE_TYPE, KEEP_ALIVE_URI):
-    template = Template(filename='./templates/ecom/keepalive.tmpl')
-    print(template.render(ip_last_octet=ip_last_octet,
-                          env=env,
-                          order=order,
-                          appname=app_name,
-                          ip=ip,
-                          ka_port=KEEP_ALIVE_PORT,
-                          ka_type=KEEP_ALIVE_TYPE,
-                          ka_uri=KEEP_ALIVE_URI))
 
 
-def print_service(ip_last_octet, ip, order, env, app_name, SERVICE_PROTOCOL):
-    template = Template(filename='./templates/ecom/service.tmpl')
-    print(template.render(ip_last_octet=ip_last_octet,
-                          env=env,
-                          order=order,
-                          appname=app_name,
-                          ip=ip,
-                          svc_proto=SERVICE_PROTOCOL))
+if __name__ == "__main__":
+
+	if len(sys.argv) != 2:
+		print("\n")
+		print("ERROR: You should enter input file path!!!\n")
+		print("USAGE: %s <config_file>\n" % sys.argv[0])
+		sys.exit(100)
+
+	try:
+		cisco = CiscoCSS(sys.argv[1])
+	except ValueError,err:
+		print err
+		sys.exit(200)
+	
+	cisco.printAll()
 
 
-def print_group(vip_last_octet, env, app_name, vip, ip_last_octet_array,
-                order_array):
-    template = Template(filename='./templates/ecom/group.tmpl')
-    print(template.render(ip_last_octet=vip_last_octet,
-                          env=env,
-                          appname=app_name,
-                          ip=vip,))
-    for i in range(len(order_array)):
-        print('  add destination service {0}_{1}{2}_{3}'
-              .format(ip_last_octet_array[i], env, order_array[i], app_name))
-    print('  active\n')
-
-
-def print_owner(vip_last_octet, vip, env, app_name, ip_last_octet_array,
-                order_array, STICKY_INACT_TIMEOUT, OWNER_PORT, OWNER_PROTOCOL,
-                REDIRECT_PORT, REDIRECT_PROTOCOL, redirect_url):
-    print('!*************************** OWNER ***************************')
-    print('owner {0}_{1}_{2}\n'.format(vip_last_octet, env, app_name))
-    print('  content {0}_{1}_{2}'.format(vip_last_octet, env, app_name))
-    print('    vip address {0}'.format(vip))
-    for i in range(len(order_array)):
-        print('    add service {0}_{1}{2}_{3}'
-              .format(ip_last_octet_array[i], env, order_array[i], app_name))
-    print('    add dns {0}'.format(redirect_url))
-    print('    sticky-inact-timeout {0}'.format(STICKY_INACT_TIMEOUT))
-    print('    port {0}'.format(OWNER_PORT))
-    print('    protocol {0}'.format(OWNER_PROTOCOL))
-    print('    advanced-balance sticky-srcip')
-    print('    active\n')
-    print('  content {0}_{1}_{2}_redir'.format(vip_last_octet, env, app_name))
-    print('    vip address {0}'.format(vip))
-    print('    sticky-inact-timeout {0}'.format(STICKY_INACT_TIMEOUT))
-    print('    port {0}'.format(REDIRECT_PORT))
-    print('    protocol {0}'.format(REDIRECT_PROTOCOL))
-    print('    redirect \"https://{0}\"'.format(redirect_url))
-    print('    active\n')
-
-
-def extract_octet(ip, octet=4):
-    '''
-       extracts an octet of an ip
-       if not specified returns the last octet
-    '''
-    import re
-    l = re.split('(.*)\.(.*)\.(.*)\.(.*)', ip)
-    return l[octet]
-
-
-def build_dict_from_file(input_file, dictionary):
-    try:
-        with open(input_file) as f:
-            for line in f:
-                if not line.isspace():
-                    (key, val) = line.split("=")
-                    dictionary[key] = val.strip('\n')
-
-    except IOError:
-        print('ERROR: File %s could not be opened!!!' % input_file)
-        sys.exit()
-
-
-def parse_input(input_file):
-    global redirect_url
-    global vip
-    global vip_last_octet
-    global app_name
-    global env
-    global ip_array
-    global ip_last_octet_array
-    global order_array
-
-    # Parse input file
-    try:
-        input_fileContent = open(input_file)
-        for line in input_fileContent:
-            if (line.find("url") != -1):
-                temp_array = line.split('=')
-                redirect_url = (temp_array[len(temp_array)-1]).rstrip()
-            if (line.find("vip") != -1):
-                temp_array = line.split('=')
-                vip = (temp_array[len(temp_array)-1]).rstrip()
-            if (line.find("name") != -1):
-                temp_array = line.split('=')
-                app_name = (temp_array[len(temp_array)-1]).rstrip()
-            if (line.find("env") != -1):
-                temp_array = line.split('=')
-                env = (temp_array[len(temp_array)-1]).rstrip()
-            if (line.find("service_ip") != -1):
-                temp_array = line.split('=')
-                ip_array.append((temp_array[len(temp_array)-1]).rstrip())
-
-        # Parse ips to retrieve the last octet
-        temp_array = vip.split('.')
-        vip_last_octet = extract_octet(vip)
-        for i in range(len(ip_array)):
-            temp_array = ip_array[i].split('.')
-            ip_last_octet_array.\
-                append((temp_array[len(temp_array)-1]).rstrip())
-            order_array.append(i+1)
-    except IOError:
-        print('ERROR: File %s could not be opened!!!' % input_file)
-        sys.exit()
-
-### Script Main ###
-if len(sys.argv) <= 1:
-    print("\n")
-    print("ERROR: You should enter input file path!!!\n")
-    print("USAGE: %s <input_file_path>" % __file__)
-else:
-    print('\n')
-    if (len(sys.argv)) == 2:
-        parse_input(str(sys.argv[1]))
-
-        # Write LB configuration to standard output
-        for i in range(len(ip_array)):
-            print_keep_alive(ip_last_octet_array[i], ip_array[i],
-                             order_array[i], env, app_name, KEEP_ALIVE_PORT,
-                             KEEP_ALIVE_TYPE, KEEP_ALIVE_URI)
-            print_service(ip_last_octet_array[i], ip_array[i], order_array[i],
-                          env, app_name, SERVICE_PROTOCOL)
-
-        print_group(vip_last_octet, env, app_name, vip, ip_last_octet_array,
-                    order_array)
-
-        print_owner(vip_last_octet, vip, env, app_name, ip_last_octet_array,
-                    order_array, STICKY_INACT_TIMEOUT, OWNER_PORT,
-                    OWNER_PROTOCOL, REDIRECT_PORT, REDIRECT_PROTOCOL,
-                    redirect_url)
-
-    else:
-        print("ERROR: You should enter only one parameter!!! <input file>.\n")
-        print("USAGE: %s <input_file_path>" % __file__)
+sys.exit(1)
